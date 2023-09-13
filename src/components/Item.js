6d60561f6d64c3f57a9ea16a147b31684c47d672
LUCY <wangecikariu.luc@gmail.com>
2023-09-13 22:32:19 +0300

    fetch

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,14 +1,31 @@
 import React from "react";
 
 function Item({ item }) {
+
+  function handleAddToCartClick () {
+    //Add fetch request
+    fetch(`http://localhost:4000/items/${item.id}`, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      isInCart: !item.isInCart,
+    }),
+  })
+    .then((r) => r.json())
+    .then((updatedItem) => console.log(updatedItem));
+    console.log ("clicked item:", item);
+  }
   return (
     <li className={item.isInCart ? "in-cart" : ""}>
       <span>{item.name}</span>
       <span className="category">{item.category}</span>
-      <button className={item.isInCart ? "remove" : "add"}>
-        {item.isInCart ? "Remove From" : "Add to"} Cart
-      </button>
-      <button className="remove">Delete</button>
+     <button className = {item.isCart ? "remove" : "add"}
+     onClick = {handleAddToCartClick} >
+      {item.isCart ? "remove From " : "add To"} Cart
+     </button>
+      <button className="remove" onClick = {handleDeleteClick}>Delete</button>
     </li>
   );
 }
